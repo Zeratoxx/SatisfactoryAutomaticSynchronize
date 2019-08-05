efe6ec0165e6e7af00f3b8dfe2a5f593d7770c3c
Nils Badtke <nils.badtke@stud.h-da.de>
2019-08-05 03:28:06 +0200

    zip support added

--- To The File/Read the README first!!/!Recommend! MoreThanOneRepository - Zip Support/To the File/Read the README first!!/SatisfactoryKomplexWithZip.bat
+++ To The File/Read the README first!!/!Recommend! MoreThanOneRepository - Zip Support/To the File/Read the README first!!/SatisfactoryKomplexWithZip.bat
@@ -22,12 +22,21 @@ SET nameOfWorldlistFile=listOfWorlds.txt
 SET counter=0
 SET listOfRepos=
 SET alreadyStarted=
+SET workWithZip=
 
 
 
 
 
 CD %PATHTOSAVED%
+
+IF EXIST %nameOfWorldlistFile% (
+	REM Go on.
+) ELSE (
+	ECHO Cannot find %nameOfWorldlistFile%. Please set it up correctly.
+	GOTO error
+)
+
 FOR /F %%f IN (%nameOfWorldlistFile%) DO (
 	
 	SET /A counter=counter+1
@@ -36,7 +45,6 @@ FOR /F %%f IN (%nameOfWorldlistFile%) DO (
 
 
 :select
-
 
 ECHO Please enter the number of the map you want to play.
 ECHO.
@@ -93,18 +101,51 @@ ECHO -----
 ECHO Loading files from %theChoicedRepo%...
 ECHO.
 
+
+
 CD %PATHTOSAVED%\SaveGames
 DEL /Q *
 
 
 CD %PATHTOSAVED%\%theChoicedRepo%
 
-if exist .\.git\ (
+IF EXIST .\.git\ (
 	git pull
+) ELSE (
+	ECHO This is not a git repository. Cannot pull.
+	ECHO Trying to use local data...
 )
-DIR /B *.sav >%PATHTOSAVED%\Logs\%theChoicedRepo%.txt
+
 ECHO.
-xcopy /q/y *.sav %PATHTOSAVED%\SaveGames\
+
+
+IF EXIST savpackage.zip (
+	SET workWithZip=y
+	ECHO Working with zip file.
+	IF EXIST %PATHTOSAVED%\temp\ (
+		REM Do nothing.
+	) ELSE (
+		MKDIR %PATHTOSAVED%\temp\
+	)
+	
+	xcopy /q/y savpackage.zip %PATHTOSAVED%\temp\
+	
+	CD %PATHTOSAVED%\temp
+	ECHO Entpacken...
+	powershell.exe -command "& { Expand-Archive savpackage.zip .\ -Force; }"
+	DIR /B *.sav >%PATHTOSAVED%\Logs\%theChoicedRepo%.txt
+	xcopy /q/y *.sav %PATHTOSAVED%\SaveGames\
+	DEL /Q *
+
+) ELSE (
+	ECHO Working with sav files.
+	DIR /B *.sav >%PATHTOSAVED%\Logs\%theChoicedRepo%.txt
+	xcopy /q/y *.sav %PATHTOSAVED%\SaveGames\
+)
+
+
+
+
 ECHO.
 ECHO.
 
@@ -156,9 +197,33 @@ ECHO.
 ECHO -----
 ECHO Saving files...
 ECHO.
+
+IF EXIST %PATHTOSAVED%\temp\ (
+	REM Do nothing.
+) ELSE (
+	MKDIR %PATHTOSAVED%\temp\
+)
+
 CD %PATHTOSAVED%\SaveGames
-xcopy /q/y *.sav %PATHTOSAVED%\%theChoicedRepo%
-DEL /Q *.sav
+
+IF DEFINED workWithZip (
+	ECHO Working with zip file.
+	xcopy /q/y *.sav %PATHTOSAVED%\temp\
+	DEL /Q *
+	
+	CD %PATHTOSAVED%\temp
+	ECHO Komprimieren...
+	powershell.exe -command "& { Compress-Archive *.sav savpackage.zip -CompressionLevel Optimal -Update; }"
+	xcopy /q/y savpackage.zip %PATHTOSAVED%\%theChoicedRepo%\
+	DEL /Q *
+	
+) ELSE (
+	ECHO Working with sav files.
+	xcopy /q/y *.sav %PATHTOSAVED%\%theChoicedRepo%\
+	DEL /Q *
+)
+
+
 DEL %PATHTOSAVED%\Logs\%theChoicedRepo%.txt
 ECHO.
 ECHO.
@@ -166,26 +231,33 @@ ECHO.
 
 
 SET gitMessage=Spielupdate %COMPUTERNAME%
-if exist %PATHTOSAVED%\%gitMessageFile% (
-FOR /F %%m IN (%PATHTOSAVED%\%gitMessageFile%) DO (
-SET gitMessage=!gitMessage! %%m
-)
+IF EXIST %PATHTOSAVED%\%gitMessageFile% (
+	FOR /F %%m IN (%PATHTOSAVED%\%gitMessageFile%) DO (
+		SET gitMessage=!gitMessage! %%m
+	)
 )
 
 
 CD %PATHTOSAVED%\%theChoicedRepo%
-if exist .\.git\ (
-git add .
-git commit -m "%gitMessage%"
-git push
+IF EXIST .\.git\ (
+	git add .
+	git commit -m "%gitMessage%"
+	git push
+) ELSE (
+	ECHO This is not a git repository. Cannot commit and push.
+	ECHO Saves are now only local data.
 )
 
 
 ECHO.
 ECHO.
 ECHO Complete. Closing...
-timeout /T 5 /nobreak >NUL
+timeout /T 10 /nobreak >NUL
 EXIT
 
-
+:error
+ECHO An error occured^^! Cancelling...
+ECHO.
+PAUSE
+EXIT
 
--- To The File/Read the README first!!/(Beta) MoreThanOneRepositoryAndZip/To the File/README.txt
+++
@@ -1,6 +0,0 @@
-Supportet mehrere Welten mit mehreren Spielständen.
-
-Die einzelnen Welten müssen in eigenen Ordnern im Ordner "Saved" sein, in denen dann u.U. auch die Git Repos sind!
-Die ORDNERNAMEN müssen in der Datei "listOfWorlds.txt" in jeweils einer Zeile stehen!
-
-Vergleiche mit dem Ordner "SampleDirStruct".
